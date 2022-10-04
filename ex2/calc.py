ccac88dd1e3f26f6afbdacaa8f20f79fbe4d1b8d
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-04 16:09:06 +0900

    四則演算表記

--- ex2/calc.py
+++ ex2/calc.py
@@ -13,28 +13,20 @@ def click_equal(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, res)
     
-
     
 
+
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x500")
 
 entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
-entry.grid(row=0, column=0, columnspan=3)
+entry.grid(row=0, column=0, columnspan=4)
 
-#練習2自分のコード
-# for i in range(9,-1,-1):
-#     button = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
-#     a = 0
-#     b = 2
-#     button.grid(a, b)
-#     if b%3 == 2:
-#         a += 1
 
 r, c = 1, 0
+shousu = ["."]
 numbers = list(range(9, -1, -1))
-operators = ["+"]
-for i, num in enumerate(numbers+operators, 1):
+for i, num in enumerate(numbers+shousu, 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
@@ -43,9 +35,17 @@ for i, num in enumerate(numbers+operators, 1):
         r += 1
         c = 0
 
-btn = tk.Button(root, text="=", font=("", 30), width=4, height=2)
-btn.bind("<1>", click_equal)
-btn.grid(row=4, column=2)
+operators = ["×", "÷", "-", "+"]
+for i, ope in enumerate(operators, 1):
+    btn = tk.Button(root, text=f"{ope}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
+    btn.grid(row=i, column=4)
+
+btn_eq = tk.Button(root, text="=", font=("", 30), width=4, height=2)
+btn_eq.bind("<1>", click_equal)
+btn_eq.grid(row=4, column=2)
+
+
         
 root.mainloop()
     
