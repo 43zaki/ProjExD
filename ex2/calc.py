8a690ff19cd1ddd1198f61bb6afe83dc283bcb06
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-04 17:16:39 +0900

    delete追加

--- ex2/calc.py
+++ ex2/calc.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def click_number(event):
+def click_number(event): #文字をつなげる関数
     btn = event.widget
     num = btn["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num) #練習5
     
-def click_equal(event):
+def click_equal(event):#イコールの処理をする関数
     eqn = entry.get()
     eqn = eqn.replace("÷", "/")
     eqn = eqn.replace("×", "*")
@@ -15,14 +15,14 @@ def click_equal(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, res)
     
-def click_del(event):
+def click_del(event):#後ろ一文字を消去する関数
     dln = entry.get()
     dln = dln[0:-1]
     entry.delete(0, tk.END)
     entry.insert(tk.END, dln)
 
 root = tk.Tk()
-root.geometry("400x500")
+root.geometry("400x500") #画面の大きさを400x500にする
 
 entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
 entry.grid(row=0, column=0, columnspan=3)
@@ -31,7 +31,7 @@ entry.grid(row=0, column=0, columnspan=3)
 r, c = 1, 0
 shousu = ["."]
 numbers = list(range(9, -1, -1))
-for i, num in enumerate(numbers+shousu, 1):
+for i, num in enumerate(numbers+shousu, 1):#数字のボタンを生成
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
@@ -41,20 +41,20 @@ for i, num in enumerate(numbers+shousu, 1):
         c = 0
 
 operators = ["×", "÷", "-", "+"]
-for i, ope in enumerate(operators, 1):
+for i, ope in enumerate(operators, 1):#+-×÷のボタンの生成
     btn = tk.Button(root, text=f"{ope}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
     btn.grid(row=i, column=3)
 
-btn_eq = tk.Button(root, text="=", font=("", 30), width=4, height=2)
+btn_eq = tk.Button(root, text="=", font=("", 30), width=4, height=2)#=のボタンの生成
 btn_eq.bind("<1>", click_equal)
 btn_eq.grid(row=4, column=2)
 
-btn_dl = tk.Button(root, text="☜×", font=("", 30), width=4, height=1)
+btn_dl = tk.Button(root, text="☜×", font=("", 30), width=4, height=1)#deleteのボタンの生成
 btn_dl.bind("<1>", click_del)
 btn_dl.grid(row=0, column=3)
 
 
         
-root.mainloop()
+root.mainloop()#画面表示
     
