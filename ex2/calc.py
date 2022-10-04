2f5625084be64e81c7705102b3025f28c71b3399
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-04 15:16:45 +0900

    練習6

--- ex2/calc.py
+++ ex2/calc.py
@@ -23,7 +23,9 @@ entry.grid(row=0, column=0, columnspan=3)
 #         a += 1
 
 r, c = 1, 0
-for i, num in enumerate(range(9,-1, -1), 1):
+numbers = list(range(9, -1, -1))
+operators = ["+"]
+for i, num in enumerate(numbers+operators, 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
