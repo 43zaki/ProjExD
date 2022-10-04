5da6a6e9a05421ced2704868db80c5ad08cad131
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-04 14:53:44 +0900

    練習4

--- ex2/calc.py
+++ ex2/calc.py
@@ -10,8 +10,10 @@ def click_number(event):
 root = tk.Tk()
 root.geometry("300x500")
 
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
 
-
+#練習2自分のコード
 # for i in range(9,-1,-1):
 #     button = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
 #     a = 0
@@ -20,7 +22,7 @@ root.geometry("300x500")
 #     if b%3 == 2:
 #         a += 1
 
-r, c = 0, 0
+r, c = 1, 0
 for i, num in enumerate(range(9,-1, -1), 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
@@ -29,4 +31,6 @@ for i, num in enumerate(range(9,-1, -1), 1):
     if i%3 == 0:
         r += 1
         c = 0
+        
+root.mainloop()
     
