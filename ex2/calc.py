67d887073ce913c4a8412727d36faf74cb28a5dd
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-04 15:30:21 +0900

    練習7

--- ex2/calc.py
+++ ex2/calc.py
@@ -3,9 +3,18 @@ import tkinter.messagebox as tkm
 
 def click_number(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = btn["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num) #練習5
+    
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
+    
+
+    
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -33,6 +42,10 @@ for i, num in enumerate(numbers+operators, 1):
     if i%3 == 0:
         r += 1
         c = 0
+
+btn = tk.Button(root, text="=", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_equal)
+btn.grid(row=4, column=2)
         
 root.mainloop()
     
