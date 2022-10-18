e1ee804e979945d1c46b9abf7f9e0ccb32b0508a
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-18 13:36:13 +0900

    7完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,11 +4,27 @@ import tkinter.messagebox as tkm
 def key_down(event):
     global key
     key = event.keysym
-    tkm.showinfo("キー押下", f"{key}キーが押されました")
+    #tkm.showinfo("キー押下", f"{key}キーが押されました")
     
 def key_up(event):
     global key
     key = ""
+    
+def main_proc():    
+    global cx, cy, key, canvas
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Left":
+        cx -= 20
+    elif key == "Right":
+        cx += 20
+        
+    canvas.coords(bird, cx, cy)
+    
+    root.after(50, main_proc)
+    
     
 
 if __name__ == "__main__":
@@ -21,11 +37,12 @@ if __name__ == "__main__":
     
     tori = tk.PhotoImage(file="pra03/fig/9.png")
     cx, cy = 300, 400
-    canvas.create_image(cx, cy, image=tori, tags="tori")
+    bird = canvas.create_image(cx, cy, image=tori, tags="tori")
     
     key = ""
     
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    main_proc()
     
     root.mainloop()
