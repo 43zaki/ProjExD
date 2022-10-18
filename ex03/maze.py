8806534b63e64b581586b7093feb8ad44fe0ace8
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-18 13:25:58 +0900

    2完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
     
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas.place(x=0, y=0)
+    
     root.mainloop()
