0618c2701ac4bc51dc81028a33dc3f513c5ec1ec
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-18 16:05:45 +0900

    ゴールとスタートの設置

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,24 +12,29 @@ def key_up(event):
     key = ""
     
 def main_proc():    
-    global cx, cy, canvas, mx, my
-    if key == "Up" and maze_source[my-1][mx] == 0:
+    global cx, cy, canvas, mx, my, key
+    if key == "Up" and maze_source[my-1][mx] != 1:
         #cy -= 20
         my -= 1
-    elif key == "Down" and maze_source[my+1][mx] == 0:
+    elif key == "Down" and maze_source[my+1][mx] != 1:
         #cy += 20
         my += 1
-    elif key == "Left" and maze_source[my][mx-1] == 0:
+    elif key == "Left" and maze_source[my][mx-1] != 1:
         #cx -= 20
         mx -= 1
-    elif key == "Right" and maze_source[my][mx+1] == 0:
+    elif key == "Right" and maze_source[my][mx+1] != 1:
         #cx += 20
         mx += 1
         
     cx = mx*100 + 50
     cy = my*100 + 50
-        
     canvas.coords(bird, cx, cy)
+    
+    if maze_source[my][mx] == 3: #ゴールしたら
+        key = ""
+        mx, my = 1, 1 #mx, myの初期化
+        tkm.showinfo("ゴール達成", "GOAL")
+    
     
     root.after(50, main_proc)
     
