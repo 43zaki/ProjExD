0ff4e58519aadee80c59268af1fc31d550610bfe
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-25 13:58:43 +0900

    4完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,6 +8,8 @@ def main():
     scrn_sfc = pg.display.set_mode((1600,900))
     bgimg = pg.image.load("ex04/fig/pg_bg.jpg")
     loop = True
+    tori_x = 900
+    tori_y = 400
     
     
     
@@ -17,6 +19,22 @@ def main():
         pg.display.update()
         
         scrn_sfc.blit(bgimg, (0,0))
+        
+        tori_sfc = pg.image.load("ex04/fig/9.png")
+        tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2)
+        tori_rct =    tori_sfc.get_rect()
+        tori_rct.center = tori_x, tori_y
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        
+        pressed_keys = pg.key.get_pressed()
+        if pressed_keys[pg.K_DOWN] and tori_rct.bottom+1 < 900:
+            tori_y += 1
+        elif pressed_keys[pg.K_UP] and tori_rct.top-1 > 0:
+            tori_y -= 1
+        elif pressed_keys[pg.K_LEFT] and tori_rct.left-1 > 0:
+            tori_x -= 1
+        elif pressed_keys[pg.K_RIGHT] and tori_rct.right+1 < 1600:
+            tori_x += 1
         
         for event in pg.event.get():
             if event.type == pg.QUIT:loop = False
