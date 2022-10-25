667624e877286df32e4a44b749a3cc55924e501c
yamazaki <c0b21155a4@edu.teu.ac.jp>
2022-10-25 16:13:50 +0900

    プログラム完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,6 +4,7 @@ import random
 
 def main():
     pg.init()
+    clock = pg.time.Clock()
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     bgimg = pg.image.load("ex04/fig/pg_bg.jpg")
@@ -12,33 +13,43 @@ def main():
     tori_y = 400
     move_x, move_y = 1, 1
     x, y = 0, 0
-    vx = random.randint(10, 1600-10)
+    vx = random.randint(10, 800-10)
     vy = random.randint(10, 900-10)
+    transmittance = 255 #透過率を表す
+    cookie_rct = 0
+    
+    pg.time.set_timer(30, 8000)
     
     
     
     while loop:
-        clock = pg.time.Clock()
+        
         clock.tick(1000)
         pg.display.update()
+        
         
         scrn_sfc.blit(bgimg, (0,0))
         
         tori_sfc = pg.image.load("ex04/fig/9.png")
         tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2)
+        tori_sfc.set_alpha((transmittance))
         tori_rct =    tori_sfc.get_rect()
         tori_rct.center = tori_x, tori_y
         scrn_sfc.blit(tori_sfc, tori_rct)
         
         pressed_keys = pg.key.get_pressed()
         if pressed_keys[pg.K_DOWN] and tori_rct.bottom+1 < 900:
-            tori_y += 1
+            y = tori_y #yとxは今の画像の座標を保存する
+            tori_y += 10
         elif pressed_keys[pg.K_UP] and tori_rct.top-1 > 0:
-            tori_y -= 1
+            y = tori_y
+            tori_y -= 10
         elif pressed_keys[pg.K_LEFT] and tori_rct.left-1 > 0:
-            tori_x -= 1
+            x = tori_x
+            tori_x -= 10
         elif pressed_keys[pg.K_RIGHT] and tori_rct.right+1 < 1600:
-            tori_x += 1
+            x = tori_x
+            tori_x += 10
             
         bomb_sfc = pg.Surface((20, 20))
         bomb_sfc.set_colorkey((0, 0, 0))
@@ -49,14 +60,56 @@ def main():
         vx += move_x
         vy += move_y
         
-        if vx-10 <= 0 or vx+10 >= 1600:
+        if vx+10 >= 1600:
+            loop = False
+        if vx-10 <= 0 :
             move_x *= -1
         if vy-10 <= 0 or vy+10 >= 900:
             move_y *= -1
+            
+        if cookie_rct != 0:
+            scrn_sfc.blit(cookie_sfc, cookie_rct)
+            if tori_rct.colliderect(cookie_rct): #クッキーとこうかとんがかさなったら
+                transmittance += 50
+                cookie_rct = 0
         
-        if tori_rct.colliderect(bomb_rct):loop = False
+        if tori_rct.colliderect(bomb_rct): #爆弾がこうかとんとかさなったら
+            #爆発の描写
+            bomb1_sfc = pg.image.load("ex04/fig/bomb1.png")
+            bomb1_sfc = pg.transform.rotozoom(bomb1_sfc, 0, 0.5)
+            bomb1_rct =    bomb1_sfc.get_rect()
+            bomb1_rct.center = tori_x, tori_y
+            scrn_sfc.blit(bomb1_sfc, bomb1_rct)
+            
+            #cookie_sfc = 0
+            
+            pg.display.update()
+            
+            #爆弾に当たったら200右に移動し爆弾の速度を上げる
+            tori_x += 200
+            if tori_x >= x:
+                move_x = abs(move_x)+0.1
+                move_x *= -1
+            
+            #透過率を上げる
+            transmittance -= 20
+            if transmittance < 15:
+                loop=False
+                
+            #爆発の画像が表示されるようにfpsを一時的に低くする。
+            clock.tick(10)
+            
         for event in pg.event.get():
             if event.type == pg.QUIT:loop = False
+            if event.type == 30:
+                cx = random.randint(800, 1600)
+                cy = random.randint(0, 900)
+                cookie_sfc = pg.image.load("ex04/fig/cookie.png")
+                cookie_sfc = pg.transform.rotozoom(cookie_sfc, 0, 0.1)
+                cookie_rct =    cookie_sfc.get_rect()
+                cookie_rct.center = cx, cy 
+                scrn_sfc.blit(cookie_sfc, cookie_rct)
+                
             
     pg.quit()
     sys.exit()
