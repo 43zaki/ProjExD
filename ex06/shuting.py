5eea199169d4eb9bbb180317fd3b5a206c3b3b9d
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-11-15 13:08:10 +0900

    最終

--- ex06/shuting.py
+++ ex06/shuting.py
@@ -146,7 +146,7 @@ def check_bound(obj_rct, scr_rct):
 
 class Deta: #ハイスコア用Txtの変更　布施
     def __init__(self):
-        self.highscore = 0
+        self.highscore = 0  #ハイスコアの初期化
     
     def Load(self,filepass):
         with open(filepass ,'r',encoding="utf8") as f:
@@ -166,14 +166,14 @@ class Deta: #ハイスコア用Txtの変更　布施
 
 class Screen_st:       #動かないスクリーン用　布施
     def __init__(self,title,wh,bgfile):
-        pg.display.set_caption(title)  
+        pg.display.set_caption(title)               #スクリーンのタイトル設定
         self.scrn_sfc=pg.display.set_mode(wh)       #スクリーン用sfc
         self.scrn_rect=self.scrn_sfc.get_rect()     #スクリーン用Rect
         self.bg_sfc =pg.image.load(bgfile)     #背景Sfc
         self.bg_rect=self.bg_sfc.get_rect()         #背景Rect 
     
     def blit(self):
-        self.scrn_sfc.blit(self.bg_sfc,self.bg_rect)
+        self.scrn_sfc.blit(self.bg_sfc,self.bg_rect)    #スクリーンの描画
         return self.scrn_sfc
 
 # png=["fig/0.png","fig/1.png","fig/2.png"]
@@ -181,35 +181,35 @@ class Screen_st:       #動かないスクリーン用　布施
 
 class Bird:           #画像読み込み、表示　布施
     def __init__(self,filename,bairitu,syokiiti):
-        self.gazou_sfc=pg.image.load(filename)
-        self.gazou_sfc= pg.transform.rotozoom(self.gazou_sfc, 0, bairitu)
-        self.gazou_rect=self.gazou_sfc.get_rect()
-        self.gazou_rect.center=(syokiiti)
+        self.gazou_sfc=pg.image.load(filename)     #画像用のSfcの作成
+        self.gazou_sfc= pg.transform.rotozoom(self.gazou_sfc, 0, bairitu)   #画像の選択と倍率の設定
+        self.gazou_rect=self.gazou_sfc.get_rect()   #画像用のRect作成
+        self.gazou_rect.center=(syokiiti)           #画像の位置を設定
     
     def blit(self,sc):
-        sc.blit(self.gazou_sfc,self.gazou_rect)
+        sc.blit(self.gazou_sfc,self.gazou_rect) #渡されたスクリーンSfcに画像の描画
 
 
 class Font:            #文字表示用　布施
-    def __init__(self,size,Non=None):
-        self.word=pg.font.Font(Non,size)
+    def __init__(self,size,Non=None):   #第一引数にフォントサイズ　第二引数にフォントの設定
+        self.word=pg.font.Font(Non,size)    #フォントとサイズの設定　第二引数が無ければNoneを返しデフォルトフォントfreesansbold.ttfを使用
         
-    def brit(self,sc,syokiiti,word,colour):
-        self.word_sfc=self.word.render(f"{word}",True,colour)
-        sc.blit(self.word_sfc,syokiiti)
+    def brit(self,sc,syokiiti,word,colour): #第一引数に描画する背景sfc、第二に位置（x、y）、第三に表示する文字、第四に色（R,G,B）
+        self.word_sfc=self.word.render(f"{word}",True,colour)   #テキストの設定
+        sc.blit(self.word_sfc,syokiiti) #背景sfcに描画
 
         
 
 def main2 ():               #スタート画面　布施
     global mod 
     score=Deta()
-    score.Load("ex06\data\Highscore.txt")
+    score.Load("ex06\data\Highscore.txt")   #ハイスコアの読み込み
     pg.init()
-    scrn_sfc=Screen_st("進撃のこうかとん",(1600,900),"ex06/data/bg.jpg")
-    bird=Bird("ex06/data/6.png",3.0,(800,450))  
-    font=Font(80)
-    font1=Font(100)
-    font2=Font(150,"SoukouMincho.ttf")
+    scrn_sfc=Screen_st("進撃のこうかとん",(1600,900),"ex06/data/bg.jpg")    #背景Sfccの作成
+    bird=Bird("ex06/data/6.png",3.0,(800,450))  #鳥Sfcの作成
+    font=Font(80)   #フォントサイズ80の準備
+    font1=Font(100) #フォントサイズ100の準備
+    font2=Font(150,"SoukouMincho.ttf")#フォントの設定とサイズの設定　フォントはSoukouMincho.ttfを指定
     keikoku=Bird("ex06/data/keikoku.jpg",1.0,(800,450)) 
     ans=0
 
@@ -224,65 +224,71 @@ def main2 ():               #スタート画面　布施
         
         
         if ans==1:
-            keikoku.blit(nsc)
+            keikoku.blit(nsc)       #ans=1の時警告画面が出るようにする
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
                 
         pressed = pg.key.get_pressed()
-        if pressed[pg.K_s]:
+        if pressed[pg.K_s]: #Sが押されたらmod=1にしゲーム画面に移行する
             mod=1
             return
-        if pressed[pg.K_c]:
+        if pressed[pg.K_c]: #Cが押されたらansを１にし警告画面が出るようにする
             ans=1
         
-        if ans==1 and pressed[pg.K_y]:
-            score.reset("ex06\data\Highscore.txt")
+        if ans==1 and pressed[pg.K_y]:          #警告画面が出ているときにYを押すと
+            score.reset("ex06\data\Highscore.txt")  #ハイスコアを初期化し表示しなおす
             ans=0
             return
         
-        if ans==1 and pressed[pg.K_n]:
+        if ans==1 and pressed[pg.K_n]:   #警告画面が出ているときにNを押すとans=0にし警告画面を非表示
             ans=0
         pg.display.update()
 
 def main3 ():              #GameOver画面　布施
     global mod,sco
     score=Deta()
-    score.Load("ex06\data\Highscore.txt")
+    score.Load("ex06\data\Highscore.txt")   #ハイスコアの読み込み
     pg.init()
-    scrn_sfc=pg.display.set_mode((1600,900))
+    scrn_sfc=pg.display.set_mode((1600,900))    #スクリーン用Sfcの作成
     pg.display.set_caption("Game Over")
-    font=Font(200)
-    font1=Font(100)
+    font=Font(200)      #フォントサイズ200の準備
+    font1=Font(100)     #フォントサイズ100の準備
     
     while True:
-        font.brit(scrn_sfc,[400,200],f"Game Over",(255,255,255))
-        if score.highscore< sco:
-            score.update("ex06\data\Highscore.txt",sco)
-            font1.brit(scrn_sfc,[500,450],f"HighScore: {sco}",(255,0,0))
+        if score.highscore< sco:    #もし今回の得点がハイスコアより高かったら
+            score.update("ex06\data\Highscore.txt",sco)     #ハイスコアを今回の得点で更新
+            font1.brit(scrn_sfc,[500,450],f"HighScore: {sco}",(255,0,0))    #ハイスコアと赤で表示
         
-        else:
-            font1.brit(scrn_sfc,[600,450],f"Score: {sco}",(255,255,255))
-        font1.brit(scrn_sfc,[500,650],f"[R] : Rstert",(255,255,255))
+        else:   #ハイスコア以下なら
+            font1.brit(scrn_sfc,[600,450],f"Score: {sco}",(255,255,255)) #スコアと白で表示
+        
+        font.brit(scrn_sfc,[400,200],f"Game Over",(255,255,255))    #テキストの表示
+        font1.brit(scrn_sfc,[500,650],f"[R] : Rstert",(255,255,255))     
+        font1.brit(scrn_sfc,[500,750],f"[E] : Exit",(255,255,255))    
+        
         pg.display.update()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-        pressed = pg.key.get_pressed()
-        if pressed[pg.K_r]:
+        pressed = pg.key.get_pressed()  
+        if pressed[pg.K_r]:  #Rが押されたらmod=0にしスタート画面へ
             mod=0
             return
-        if pressed[pg.K_e]:
+        if pressed[pg.K_e]: #Eが押されたらプログラムの終了
             pg.quit()
             sys.exit()
 
            
 def main():
     global mod ,sco
-    sco=0
+    sco=0  #今回の得点用
+    hiscore=Deta()  #ハイスコアの読み込み
+    font=Font(50)   #ハイスコア表示用
+    hiscore.Load("ex06\data\Highscore.txt")
     start = 0 # 無敵時間のスタートの初期値
     item_time = 0 # 無敵時間の初期値 エラーを起こさないため
     balet = 50 # playerが撃てる球の数
@@ -318,6 +324,7 @@ def main():
         scrn.update()
         group.update(scrn.sfc)
         group.draw(scrn.sfc)
+        font.brit(scrn.sfc,[1300,10],f"Highscore: {hiscore.highscore}",(255,0,0))   #ハイスコアの表示
         
         if pg.sprite.spritecollide(player, item_grp , dokill=True):
             # playerがアイテムと接触したとき、
@@ -349,7 +356,7 @@ def main():
                 player_grp.remove(thunder)
             elif pg.sprite.spritecollide(player, enemy_grp , dokill=False): 
                 # playerとenemyが衝突したら終了
-                mod=2 #モード変更
+                mod=2 #モード変更　（ゲームオーバー画面へ切り替え）
                 return
             if pg.sprite.groupcollide(player_grp, enemy_grp, dokilla=going, dokillb=True):
                 # playerの球と敵が衝突したときにスコアを更新する
